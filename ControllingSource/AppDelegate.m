1c6c2706a6bfdb1614bfa02d6ac73a77b2d6c48b
Luca Giordano <giord.luca@gmail.com>
2014-09-08 15:39:53 +0200

    commit appdelegate nsLog su provaLuca

--- ControllingSource/AppDelegate.m
+++ ControllingSource/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"application su provaLuca");
     // Override point for customization after application launch.
     UINavigationController *navigationController = (UINavigationController *)self.window.rootViewController;
     MasterViewController *controller = (MasterViewController *)navigationController.topViewController;
